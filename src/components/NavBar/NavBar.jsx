496f03e1c2921f6f57f4db3b25b5f8880b531b9c
unknown <carlosdanielrod@gmail.com>
2021-08-20 07:51:34 -0500

    Second challenge

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <nav className="nav-bar">
+            <img src="https://picsum.photos/200" alt=""/>
+            <ul>
+                <li>Estimator</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
