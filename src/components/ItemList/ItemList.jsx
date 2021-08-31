cc16b19a8b24c348700112486f38be90d6b99e2f
unknown <carlosdanielrod@gmail.com>
2021-08-31 08:34:09 -0500

    Catalogue corrections

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,27 +1,9 @@
-import { useState, useEffect } from 'react'
 import ItemCount from '../ItemCount/ItemCount'
 import Item from '../Item/Item'
 import Grid from '@material-ui/core/Grid'
-import productsList from '../../assets/datamock'
 
-let fakeApiCall = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Calling fake API")
-            resolve(productsList)
-        }, 2000)
-        // reject('Call to fake API failed')
-    })
-}
-const ItemList = () => {
-    const addToCart = () => console.log('Added to cart')
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        fakeApiCall()
-            .then((res) => { console.log(res); setProducts(res) })
-            .catch(e => console.log(e))
-    }, [])
-    return (
+const ItemList = ({onAdd, products}) => {
+        return (
         <Grid container spacing={3}>
             {products.map((item) => (
                 <Grid item xs={3} key={item.productId}>   
@@ -33,7 +15,7 @@ const ItemList = () => {
                     <ItemCount
                         stock={item.productStock}
                         initial="0"
-                        onAdd={addToCart}
+                        onAdd={onAdd}
                     />
                 </Grid>
             ))}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react'
 import ItemList from '../ItemList/ItemList'
+import productsList from '../../assets/datamock'
 
+let fakeApiCall = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Calling fake API")
+            resolve(productsList)
+        }, 2000)
+    })
+}
+    
 function ItemListContainer ({greeting}) {
+    const addToCart = () => console.log('Added to cart')
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fakeApiCall()
+            .then((res) => { console.log(res); setProducts(res) })
+            .catch(e => console.log(e))
+    }, [])
     return (
         <div className="item-list-container">
             <h1>{greeting}</h1>
-            <ItemList />
+            <ItemList 
+                onAdd={addToCart}
+                products={products}
+            />
         </div>
     );
 }
